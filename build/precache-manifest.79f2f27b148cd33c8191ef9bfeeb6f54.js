f29ca42bab17876b7439ef148298d1ef73470b2e
igniroits <w.hasithasenanayaka@gmail.com>
2020-06-11 10:13:45 +0530

    Update the Agora Code

---
+++ build/precache-manifest.79f2f27b148cd33c8191ef9bfeeb6f54.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50684f599c385bc9eb773c5f09035440",
+    "url": "./dd4dcb19555b94c1c5ce.worker.js"
+  },
+  {
+    "revision": "77efc4a204234d078cb3a7190e8cac5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b455091997dd7617371",
+    "url": "./static/css/2.5b79b3b6.chunk.css"
+  },
+  {
+    "revision": "205d2ee9e8c3fb7f4e8b",
+    "url": "./static/css/main.6d357d60.chunk.css"
+  },
+  {
+    "revision": "4b455091997dd7617371",
+    "url": "./static/js/2.153a7e41.chunk.js"
+  },
+  {
+    "revision": "74b1641c45751fd85daa4ca5c1c4257e",
+    "url": "./static/js/2.153a7e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205d2ee9e8c3fb7f4e8b",
+    "url": "./static/js/main.4912cfce.chunk.js"
+  },
+  {
+    "revision": "aed56733dd6df7d15277",
+    "url": "./static/js/runtime-main.57648836.js"
+  },
+  {
+    "revision": "2a72994b8a5a34fcb92ba89ffd862c4b",
+    "url": "./static/media/cover-cn.2a72994b.png"
+  },
+  {
+    "revision": "a2d244ac024c8c7f67a3a849597a5ba8",
+    "url": "./static/media/cover-us.a2d244ac.png"
+  },
+  {
+    "revision": "45b9371b21ff0d4e1ce607bc647ba267",
+    "url": "./static/media/pic-message@2x.45b9371b.png"
+  },
+  {
+    "revision": "f5d3055bb39ea0d98e47435a207b7040",
+    "url": "./static/media/pic-student.f5d3055b.png"
+  },
+  {
+    "revision": "580ff5f1a224a4b75cf57361e7f0d661",
+    "url": "./static/media/pic-teacher.580ff5f1.png"
+  }
+]);
